b162551199161424433925875abe27d4f0384b07
JochemBrandsema <jochem.b8@gmail.com>
2024-05-23 23:45:09 +0200

    torch profiler

--- decode_interactive.py
+++ decode_interactive.py
@@ -1,4 +1,8 @@
 from fairseq_easy_extend_cli import interactive
+from torch.profiler import profile, record_function, ProfilerActivity
 
 if __name__ == "__main__":
-    interactive.cli_main()
+    with profile(profile_memory=True) as prof:
+        with record_function("decoding"):
+            interactive.cli_main()
+    print(prof.key_averages())
