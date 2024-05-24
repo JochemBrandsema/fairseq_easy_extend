575acdc4a7105bc8920b3289a6c5e49337bd98e6
JochemBrandsema <jochem.b8@gmail.com>
2024-05-24 23:17:16 +0200

    too much overhead

--- decode_interactive.py
+++ decode_interactive.py
@@ -1,8 +1,4 @@
-from torch.profiler import profile, record_function
 from fairseq_easy_extend_cli import interactive
 
 if __name__ == "__main__":
-    with profile(profile_memory=True) as prof:
-        with record_function("decoding"):
-            interactive.cli_main()
-    print(prof.key_averages())
+    interactive.cli_main()
